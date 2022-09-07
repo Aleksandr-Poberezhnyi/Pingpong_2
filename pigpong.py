be633dad0f3fa57abaa2e0ceb3b84cec3abec60a
Aleksandr-Poberezhnyi <99130828+Aleksandr-Poberezhnyi@users.noreply.github.com>
2022-09-07 18:56:43 +0300

    Update pigpong.py

--- pigpong.py
+++ pigpong.py
@@ -13,6 +13,28 @@ class Sprite_(sprite.Sprite):
     def reset(self):
         win.blit(self.image,(self.rect.x,self.rect.y))
 
+back = (200, 255, 255) #Цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+#флаги отвечающие за состояние игры
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+#Создание мяча и ракетки
+racket1 = Player('racket.png', 30, 200, 4, 50, 150) #При создании спрайта добавляться ещё два параметра
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 150)
+
+font.init()
+font =font.Font('calibri.ttf', 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+        
 while game:
     for e in event.get():
         if e.type == quit:
