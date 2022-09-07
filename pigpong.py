6c930c6442008785e589d09df8065a2e197643cf
Aleksandr-Poberezhnyi <99130828+Aleksandr-Poberezhnyi@users.noreply.github.com>
2022-09-07 19:07:13 +0300

    Update pigpong.py

--- pigpong.py
+++ pigpong.py
@@ -1,6 +1,6 @@
 from pygame import *
 
-class Sprite_(sprite.Sprite):
+class GameSprite(sprite.Sprite):
     def __init__(self,player_image,player_x,player_y,width,height):
         super().__init__()
         
@@ -12,6 +12,20 @@ class Sprite_(sprite.Sprite):
         self.rect.y = player_y
     def reset(self):
         win.blit(self.image,(self.rect.x,self.rect.y))
+        
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
 back = (200, 255, 255) #Цвет фона (background)
 win_width = 600
