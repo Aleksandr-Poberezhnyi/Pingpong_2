31f1e53fb6611e39ee21bf0de5a3e4e908898d23
Aleksandr-Poberezhnyi <99130828+Aleksandr-Poberezhnyi@users.noreply.github.com>
2022-09-07 18:53:13 +0300

    Update pigpong.py

--- pigpong.py
+++ pigpong.py
@@ -12,3 +12,36 @@ class Sprite_(sprite.Sprite):
         self.rect.y = player_y
     def reset(self):
         win.blit(self.image,(self.rect.x,self.rect.y))
+
+while game:
+    for e in event.get():
+        if e.type == quit:
+            GAME = False
+    if finish != True:
+        window.fill(back)
+        racket1.update_r()
+        racket.update_l()
+        ball.rect.x += speed_x 
+        ball.rect.y += speed_y 
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+        
+        #если мяч улетел дальши ракетки, выводим условие программы для первого игрока 
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        #если мяч улетел дальши ракетки, выводим условие программы для второго игрока 
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+    display.update()
+    clock.tick(FPS)
